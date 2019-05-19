b1e895f4d6e26ec5e300195f66159f5bb2d913ac
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-05-19 11:44:40 +0300

    refactoring

--- Freechat/userserver.h
+++ Freechat/userserver.h
@@ -10,20 +10,9 @@
 #include <QTcpServer>
 #include <QHostAddress>
 
-class Connection;
-
 class UserServer : public QTcpServer
 {
-    Q_OBJECT
 
-public:
-        UserServer(QObject *parent = nullptr);
-
-signals:
-        void NewConnection(Connection *connection);
-
-protected:
-        void InComingConnection(qintptr socketDescription) override;
 };
 
 #endif // USERSERVER_H
