c8ee030714b2ef745326fe7721e6d11e9f250e6d
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-06-01 19:26:46 +0300

    backup

---
+++ Freechat/connectionf2f.cpp
@@ -0,0 +1,44 @@
+/*
+***Copyleft (C) 2019 Softwater, Inc
+***Contact: bogdyname@gmail.com
+*/
+
+#include "usernametable.h"
+#include "connectionf2f.h"
+#include "username.h"
+
+ConnectionF2F::ConnectionF2F(QObject *parent)
+    : QTcpSocket(parent)
+{
+
+}
+
+void ConnectionF2F::DigitalSignature()
+{
+
+}
+
+void ConnectionF2F::ConnectingToPeer()
+{
+
+}
+
+void ConnectionF2F::SocketConnected()
+{
+
+}
+
+void ConnectionF2F::SocketDisconnected()
+{
+
+}
+
+void ConnectionF2F::SockedErroe()
+{
+
+}
+
+void ConnectionF2F::MakeSocket()
+{
+
+}
