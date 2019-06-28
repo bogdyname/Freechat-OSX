8ebb8343dac4bf5ec8727408f109bd6d60fcc945
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-06-28 17:00:53 +0300

    Update connectionf2f.h

--- Freechat/Network/connectionf2f.h
+++ Freechat/Network/connectionf2f.h
@@ -3,7 +3,7 @@
 ***Contact: bogdyname@gmail.com
 */
 
-#include "User/username.h"
+#include "Bin/bin.h"
 #include <QNetworkInterface>
 #include <QAbstractSocket>
 #include <QNetworkSession>
@@ -15,84 +15,65 @@
 #include <QtNetwork>
 #include <QSsl>
 
-using namespace QSsl;
-using namespace QPasswordDigestor;
-
 #ifndef CONNECTIONF2F_H
 #define CONNECTIONF2F_H
-class ConnectionF2F : public QTcpSocket, Usernametable
+class ConnectionF2F : public QTcpSocket
 {
     Q_OBJECT
+
+    friend class Bin;
 
 public:
     ConnectionF2F(QObject *parent = nullptr);
     ~ConnectionF2F();
 
-private slots:
-    void DigitalSignature();
     void ConnectingToPeer();
-    void SocketConnected();
-    void SocketDisconnected();
     void ReadyReadOfData();
 
-private:
-    quint16 GetPeerPort();
-    QHostAddress GetPeerAddress();
-
-private:
-    QTimerEvent TimerForWaitingConnection();
-
+    void AskForConnectingToPortPeer();
+    void AskForDisconnectingFromPortPeer();
 };
 #endif
 
-#ifndef CLIENT_H
-#define CLIENT_H
-class Client : public ConnectionF2F
+#ifndef PEEROUT
+#define PEEROUT
+class Peerout : public ConnectionF2F
 {
     Q_OBJECT
 
-public:
-    Client(QObject *parent = nullptr);
-    ~Client();
-
-    void GetSocketFromClient();
-
-private:
-    void MakeSocketClient();
-    void SockedErroe(int socketError, const QString &message);
+    friend void GetIpAddressFromWAN(QString &textWithIPAddres);
 
 public:
-    quint16 GetPeerPort();
-    QHostAddress GetPeerAddress();
+    Peerout();
+    ~Peerout();
 
 private:
-    quint16 PeerPort(quint16 port);
-    QHostAddress PeerAddress(const QString &hostName);
+    void MakeSocket();
+    void SocketError();
+    void SocketConnected();
+    void SocketDisconnected();
 
 private:
-    QString hostName;
-    quint16 port;
-    QMutex mutex;
-    QWaitCondition cond;
-    bool quit;
+    QTcpSocket *socket = nullptr;
 };
 #endif
 
-#ifndef HOST_H
-#define HOST_H
-class Host : public ConnectionF2F
+#ifndef PEERONSIDE
+#define PEERONSIDE
+class Peerinside : public ConnectionF2F
 {
     Q_OBJECT
 
-public:
-    Host(QObject *parent = nullptr);
-    ~Host();
-
-    void GetSocketFromHost();
+    Peerinside();
+    ~Peerinside();
 
 private:
-    void MakeSocketHost();
-    void SockedErroe(int socketError, const QString &message);
-    void ListenPort();
+    void MakeSocket();
+    void SocketError();
+    void SocketConnected();
+    void SocketDisconnected();
+
+private:
+    QTcpSocket *socket = nullptr;
 };
 #endif
