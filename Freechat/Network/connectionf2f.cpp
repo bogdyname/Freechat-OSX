ea786bee6b0e9a83a7dffe6198ec3009f60d8640
bogdyname <43905628+bogdyname@users.noreply.github.com>
2019-07-01 15:03:08 -0500

    Update connectionf2f.cpp

--- Freechat/Network/connectionf2f.cpp
+++ Freechat/Network/connectionf2f.cpp
@@ -2,7 +2,6 @@
 ***Copyleft (C) 2019 Softwater, Inc
 ***Contact: bogdyname@gmail.com
 */
-
 #include "Network/connectionf2f.h"
 #include "Bin/bin.h"
 
@@ -12,6 +11,8 @@ ConnectionF2F::ConnectionF2F(QObject *parent)
 
 }
 
+/////////////////////////////////////////////////////
+
 Peerout::~Peerout()
 {
     delete socket;
@@ -19,8 +20,9 @@ Peerout::~Peerout()
 
 void Peerout::MakeSocket()
 {
+    GetIpAddressFromWAN(strWANip);
     socket = new QTcpSocket(this);
-    socket->connectToHost("WAN IP OF USER", 80);
+    socket->connectToHost(strWANip, 80);
 
         if(socket->waitForConnected(3000))
         {
@@ -40,3 +42,40 @@ void Peerout::MakeSocket()
             qDebug() << "Not connected!";
         }
 }
+
+void Peerout::GetIpAddressFromWAN(QString &textWithIPAddres)
+{
+        QNetworkAccessManager networkManager;
+        QHostAddress IP;
+
+        QUrl url("https://api.ipify.org");
+        QUrlQuery query;
+        query.addQueryItem("format", "json");
+        url.setQuery(query);
+
+        QNetworkReply* reply = networkManager.get(QNetworkRequest(url));
+
+        connect(reply, &QNetworkReply::finished, [&]()
+        {
+            if(reply->error() != QNetworkReply::NoError)
+            {
+                qDebug() << "error: " << reply->error();
+            }
+            else
+            {
+                QJsonObject jsonObject= QJsonDocument::fromJson(reply->readAll()).object();
+                QHostAddress ip(jsonObject["ip"].toString());
+
+                IP = ip;
+            }
+            reply->deleteLater();
+        }
+        );
+
+        textWithIPAddres = IP.toString();
+
+        return;
+}
+
+
+/////////////////////////////////////////////////////////////////
