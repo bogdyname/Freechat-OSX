88683a060e7e7171d2c2980c83d5252306dea605
bogdyname <bogdyname@gmail.com>
2019-10-22 19:52:41 +0300

    update

--- test/Bin/freechat.h
+++ test/Bin/freechat.h
@@ -29,17 +29,13 @@ public:
     static QString nickNameOfPeer;
     static QString bufferOfMessages;
 
-private:
-    int (*checkNetworkConnection)() = nullptr;
-    bool (*checkConnection)() = nullptr;
-
 public:
     explicit Freechat(QWidget *parent = nullptr);
     ~Freechat();
 
 private slots:
-    void on_showNetworkInfo_clicked();
-    void on_connectionToPeer_clicked();
+    void on_showNetworkInfo_clicked(bool checked);
+    void on_connectionToPeer_clicked(bool checked);
 
 private slots:
     void on_lineForTypeText_returnPressed();
--- test/Network/connectionf2f.h
+++ test/Network/connectionf2f.h
@@ -31,6 +31,5 @@ public:
 
 public:
     void NetworkInfo();
-    static int CheckConnection();
 };
 #endif
